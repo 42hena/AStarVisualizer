2116529dcf8001de000f5a4ddba3b2919d32755d
soloplay <somnium1108@gmail.com>
2025-08-27 20:02:28 +0900

    Vector2 구현
    - 위치를 나타낼 객체 구현

---
+++ AStar/Vector2.cpp
@@ -0,0 +1,121 @@
+﻿#include "Vector2.h"
+
+#include <iostream>
+
+#pragma region STATIC_맴버_초기화
+Vector2 Vector2::Zero = Vector2(0, 0);
+Vector2 Vector2::One = Vector2(1, 1);
+Vector2 Vector2::Up = Vector2(0, 1);
+Vector2 Vector2::Left = Vector2(-1, 0);
+Vector2 Vector2::Right = Vector2(1, 0);
+Vector2 Vector2::Down = Vector2(0, -1);
+#pragma endregion
+
+
+/*
+*		Vector2의 특수 맴버 함수
+*/
+
+Vector2::Vector2(int x, int y)
+	:_x(x), _y(y)
+{
+
+}
+
+/*
+*		Operator 연산자 오버로딩
+*/
+
+/* 사칙 연산 */
+
+Vector2 Vector2::operator+(const Vector2& rhs) const {
+	return Vector2(_x + rhs._x, _y + rhs._y);
+}
+
+Vector2 Vector2::operator-(const Vector2& rhs) const {
+	return Vector2(_x - rhs._x, _y - rhs._y);
+}
+
+Vector2 Vector2::operator*(const Vector2& rhs) const {
+	return Vector2(_x * rhs._x, _y * rhs._y);
+}
+
+Vector2 Vector2::operator/(const Vector2& rhs) const {
+	return Vector2(_x / rhs._x, _y / rhs._y);
+}
+
+/* 비교 연산 */
+
+bool Vector2::operator==(const Vector2& rhs) const
+{
+	if ((_x - rhs._x) | _y - rhs._y) {
+		return false;
+	}
+	return true;
+}
+
+bool Vector2::operator!=(const Vector2& rhs) const
+{
+	return !operator==(rhs);
+}
+
+/* 대입 연산 */
+
+Vector2& Vector2::operator+=(const Vector2& rhs)
+{
+	_x += rhs._x;
+	_y += rhs._y;
+	return *this;
+}
+Vector2& Vector2::operator-=(const Vector2& rhs)
+{
+	_x -= rhs._x;
+	_y -= rhs._y;
+	return *this;
+}
+Vector2& Vector2::operator*=(const Vector2& rhs)
+{
+	_x *= rhs._x;
+	_y *= rhs._y;
+	return *this;
+}
+Vector2& Vector2::operator/=(const Vector2& rhs)
+{
+	_x /= rhs._x;
+	_y /= rhs._y;
+	return *this;
+}
+
+/*
+*		공개 함수
+*/
+
+const char* Vector2::ToString()
+{
+	static char buffer[256];
+	sprintf_s(buffer, 256, "(%d, %d)", _x, _y);
+
+	return buffer;
+}
+
+/* 사칙 연산 함수 */
+
+Vector2 Vector2::Add(const Vector2& rhs) const
+{
+	return Vector2(_x + rhs._x, _y + rhs._y);
+}
+
+Vector2 Vector2::Substract(const Vector2& rhs) const
+{
+	return Vector2(_x - rhs._x, _y - rhs._y);
+}
+
+Vector2 Vector2::Multiply(const Vector2& rhs) const
+{
+	return (*this) * rhs;
+}
+
+Vector2 Vector2::Divide(const Vector2& rhs) const
+{
+	return (*this) / rhs;
+}
---
+++ AStar/Vector2.h
@@ -0,0 +1,58 @@
+﻿#ifndef __VECTOR2_H__
+#define __VECTOR2_H__
+
+#include <Windows.h>
+
+class Vector2
+{
+#pragma region 특수 맴버 함수
+public:
+    Vector2(int x = 0, int y = 0);
+    ~Vector2() = default;
+
+    Vector2(const Vector2& rhs)				= default;
+    Vector2(Vector2&& rhs)                  = default;
+    Vector2& operator=(const Vector2& rhs)	= default;
+    Vector2& operator=(Vector2&& rhs)       = default;
+#pragma endregion
+
+public:
+    Vector2 operator+(const Vector2& rhs) const;
+    Vector2 operator-(const Vector2& rhs) const;
+    Vector2 operator*(const Vector2& rhs) const;
+    Vector2 operator/(const Vector2& rhs) const;
+
+    bool operator==(const Vector2& rhs) const;
+    bool operator!=(const Vector2& rhs) const;
+
+    Vector2& operator+=(const Vector2& rhs);
+    Vector2& operator-=(const Vector2& rhs);
+    Vector2& operator*=(const Vector2& rhs);
+    Vector2& operator/=(const Vector2& rhs);
+
+public:
+    const char* ToString();
+
+public:
+    Vector2 Add(const Vector2& rhs) const;
+    Vector2 Substract(const Vector2& rhs) const;
+    Vector2 Multiply(const Vector2& rhs) const;
+    Vector2 Divide(const Vector2& rhs) const;
+
+
+public:
+    static Vector2 Zero;
+    static Vector2 One;
+    static Vector2 Up;
+    static Vector2 Left;
+    static Vector2 Right;
+    static Vector2 Down;
+
+#pragma region 맴버 변수
+public:
+    int _x = 0;
+    int _y = 0;
+#pragma endregion
+};
+
+#endif
