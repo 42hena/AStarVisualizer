1d331308031c573b1d5f49a91c7e7e4ccac677ba
soloplay <somnium1108@gmail.com>
2025-08-27 23:17:36 +0900

    GDIManager 객체 생성
    - Brush만 삽입, 나머지는 나중에 추가

---
+++ GDIManager.cpp
@@ -0,0 +1,64 @@
+﻿#include "GDIManager.h"
+
+GDIManager* GDIManager::_pStatic_instance = nullptr;
+
+/*
+*		GDIManager의 특수 맴버 함수
+*/
+
+GDIManager::~GDIManager()
+{
+}
+
+/*
+*		static 함수
+*/
+
+GDIManager* GDIManager::GetInstance()
+{
+    return _pStatic_instance;
+}
+
+void GDIManager::Create()
+{
+    if (_pStatic_instance != nullptr) {
+        return;
+    }
+    _pStatic_instance = new GDIManager();
+}
+
+void GDIManager::Destroy()
+{
+    if (_pStatic_instance == nullptr) {
+        return;
+    }
+
+    // 삭제
+    delete _pStatic_instance;
+    _pStatic_instance = nullptr;
+}
+
+/*
+*		요청 함수
+*/
+
+void GDIManager::AddBrush(std::string name, HBRUSH brush)
+{
+    auto it = _brushes.find(name);
+    if (it != _brushes.end()) {
+        return;
+    }
+
+    // 추가
+    _brushes[name] = brush;
+}
+
+HBRUSH GDIManager::GetBrush(std::string name) const
+{
+    auto it = _brushes.find(name);
+    if (it == _brushes.end()) {
+        return nullptr;
+    }
+
+    return it->second;
+}
---
+++ GDIManager.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include <unordered_map>
+#include <string>
+#include <Windows.h>
+
+class GDIManager
+{
+private:
+	GDIManager() = default;
+	~GDIManager();
+public:
+	GDIManager(const GDIManager&)				= delete;
+	GDIManager(GDIManager&&)					= delete;
+	GDIManager& operator=(const GDIManager&)	= delete;
+	GDIManager& operator=(GDIManager&&)			= delete;
+
+public:
+	static GDIManager* GetInstance();
+	static void Create();
+	static void Destroy();
+
+	void AddBrush(std::string name, HBRUSH brush);
+	HBRUSH GetBrush(std::string name) const;
+
+private:
+	static GDIManager* _pStatic_instance;
+private:
+	std::unordered_map<std::string, HBRUSH> _brushes;
+};
